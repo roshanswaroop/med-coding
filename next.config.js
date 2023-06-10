635eb1e0463009ba1dd027426628114490662cb2
KachachanC <kachachanc@gmail.com>
2023-06-09 23:15:52 -0700

    Robust Secrets Management with Onboardbase
    - added custom node backend server
    - added secrets management via onboardbase
    - fixed some namespace bugs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve = {
+      ...config.resolve,
+      fallback: {
+        fs: false,
+        child_process: false,
+      },
+    };
+    return config;
+  },
   async redirects() {
     return [
       {
